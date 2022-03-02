57a71b4a906001a3b261af5d0e01f657425c8b7a
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2022-03-02 17:01:14 +0600

    Create mahdi.py

---
+++ mahdi.py
@@ -0,0 +1,75 @@
+import requests
+
+from requests.structures import CaseInsensitiveDict
+
+import os
+
+import sys
+
+import time
+
+os.system("pip install requests")
+
+os.system("clear")
+
+red="\033[0;31m"          # Red
+
+yellow="\033[0;33m"       # Yellow
+
+green="\033[0;32m"        # Green
+
+color_off="\033[0m"       # Text Reset
+
+bblack="\033[1;30m"       # Black
+
+bred="\033[1;31m"         # Red
+
+ured="\033[4;31m"         # Red
+
+on_green="\033[42m"       # Green
+
+blue="\033[0;34m"         # Blue
+
+lightblue = '\033[94m'
+
+red = '\033[91m'
+
+white = '\33[97m'
+
+yellow = '\33[93m'
+
+green = '\033[1;32m'
+
+cyan  = "\033[96m"
+
+end = '\033[0m'
+
+purple="\033[0;35m"
+
+def main():
+	os.system("clear")
+print("""
+  ########   ##       ## ##      ##    ##   ###### ##########
+  ##            ##    ## ##      ##    ##   ##         ##
+ ##              ###     ##      ##    ##   ##         ##
+  ########        ##     ##      ########   ######     ##
+       ##         ##     ##      ##    ##  ##         ##                                   
+       ##         ##     ##      ##    ##   ##         ##
+ ########         ##     ##########    ##   ######     ##
+
+########        ####     ####       #### #######
+##             ##   ##   ##  ##     ##   ##
+## #########   ##    ##  ##    ##   ##   ############
+##    ##  ##  ## ####### ##     ##  ##   ##    ##  ##
+########  ## ##       ## ##      ####    ########  ##
+ 
+____________________________________________________________
+  \33[93mAUTHOR :\033[91m[MAHDI HASAN] SHUVO
+   \033[0;33mGITHUB : \033[1;97mhttps://github.com/Shuvo-BBHH
+   WELL COME SYLHET GAMG (IAM NEW MEMBER IN YOUR GRUP)
+  LIVE in Sylhet (Read in class 10)
+\033[42mNo NEED GF \033[0;31mIF YOU LOVE ME I LOVE YOU IF U HAT ME I FUCK YOU 
+____________________________________________________________
+""")
+
+
