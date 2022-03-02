49352448f8a730c268e6f086611a8e4be7d8a18e
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2022-03-02 17:05:29 +0600

    Update mahdi.py

--- mahdi.py
+++ mahdi.py
@@ -48,15 +48,15 @@ purple="\033[0;35m"
 
 def main():
 	os.system("clear")
-print("""
+print("""\033[0;31m
   ########   ##       ## ##      ##    ##   ###### ##########
-  ##            ##    ## ##      ##    ##   ##         ##
+   ##            ##    ##  ##      ##    ##   ##         ##
  ##              ###     ##      ##    ##   ##         ##
-  ########        ##     ##      ########   ######     ##
-       ##         ##     ##      ##    ##  ##         ##                                   
+  ########         ##     ##      ########   ######     ##
+                     ##         ##     ##      ##    ##   ##         ##  
        ##         ##     ##      ##    ##   ##         ##
  ########         ##     ##########    ##   ######     ##
-
+\033[0;32m
 ########        ####     ####       #### #######
 ##             ##   ##   ##  ##     ##   ##
 ## #########   ##    ##  ##    ##   ##   ############
@@ -68,8 +68,33 @@ ____________________________________________________________
    \033[0;33mGITHUB : \033[1;97mhttps://github.com/Shuvo-BBHH
    WELL COME SYLHET GAMG (IAM NEW MEMBER IN YOUR GRUP)
   LIVE in Sylhet (Read in class 10)
-\033[42mNo NEED GF \033[0;31mIF YOU LOVE ME I LOVE YOU IF U HAT ME I FUCK YOU 
+\033[42mNo NEED GF \033[0;31mIF YOU LOVE ME I LOVE YOU IF U HAT ME I FUCK YOU ♬♬♬
 ____________________________________________________________
 """)
+print("""\033[0;32mGRUP ADMIN:
+\033[0;31mADMIN FB ID :Radi Ahmed
+\033[0;32mADMIN FB PAGE :Ahia Ahmed
+\033[0;31mDAVOLAPER : MAHDI HASAN SUVO
+""")
+print("\033[0;35m  [1] CONTRACT ME")
+print("\033[42m [2] JOYENT GRUP")
+print("\033[91m  [3] GRUP ADMIN ID")
+print("\033[91m  [4] GRUP PAGE")
+def log_sel():
+	sel = raw_input("("\033[42m CHOOSE: ("\033[42m")
+	if sel == "":
+		print("\t\033[91;1m  SELECT AN OPTION STUPID -_")
+		log_sel()
+	elif sel =="1" or sel =="01":
+		  os.system('xdg-open https://www.facebook.com/mahdi.80808')
 
+	elif sel =="2" or sel =="02":
+		  os.system('xdg-open https://www.facebook.com//groups/924498548445689')
+		main()
+	elif sel =="3" or sel =="03":
+		import os.system('xdg-open https://www.facebook.com/Radi.Ahmed.official')
 
+	log_sel()
+	elif sel =="4" or sel =="04":
+		  os.system('xdg-open https://www.facebook.com/Ahia.Dadu.official')
+print("\033[91m FRIEST JOYENT SYHLER GANG TM SO, I MAKE THIS TOO YOU CAN USE THIS TOOL FREE")
